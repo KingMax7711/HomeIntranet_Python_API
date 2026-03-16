18440dcd2cf04bc1e41e34977b6bd5d7e947e8e6
Maxime <maxime.czegledi@gmail.com>
2026-03-16 16:48:24 +0100

    Hotfix (Agrandissement de la pool SQL)

--- database.py
+++ database.py
@@ -16,6 +16,9 @@ URL_DATABASE = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NA
 engine = create_engine(
 	URL_DATABASE,
 	pool_pre_ping=True,
+    pool_size=10,
+	max_overflow=20,
+    pool_timeout=30,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
